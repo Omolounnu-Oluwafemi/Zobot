e3e271a09b5e8a012bf081601893501aaa1933f9
Oluwafemi <oluwafemiomolounnu@gmail.com>
2024-02-29 23:26:08 +0100

    testing

---
+++ index.js
@@ -0,0 +1,12 @@
+function showBulb() {
+    document.getElementById("bulb").style.display = "block";
+    document.getElementById("welcome").style.display = "none";
+}
+
+function editText() {
+    document.getElementById('welcome').style.color = "green";
+}
+
+function handleTele() {
+    document.getElementById('tele').style.color = "blue";
+}
